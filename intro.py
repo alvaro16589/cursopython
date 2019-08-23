3d8eb80982d703ff2eda825e316987ba999c5d5f
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2019-08-23 19:55:48 -0400

    asdf

--- intro.py
+++ intro.py
@@ -1,1 +1,3 @@
-print("esta es una prueba")
+a = int(input("introdusca un valor : "))
+b = int(input("introdusca otro valor  : "))
+print("esta es una prueba cuyo resul  tados es  : ", a + b)
---
+++ tarea.py
@@ -0,0 +1,3 @@
+a = int(input("Introduzca un valor  : "))
+b = int(input("Introduzca un valor  : "))
+print("la suma es : ",a+b,"\nla resta es : ",a-b,"\nla division : ",a/b,"\nla multiplicacion es : ",a*b)
