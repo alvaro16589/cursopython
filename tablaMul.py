1e1d5e2e41ca549013540115a52f07b16783cc97
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2019-08-30 18:48:57 -0400

    modificando tabla de multiplicar

--- tablaMul.py
+++ tablaMul.py
@@ -1,4 +1,7 @@
 #la tabla de multiplicar
 a = int(input("Se imprimirá la tabla de multiplicar de : "))
 for i in range (1,11):
-    print(a, " x ", i ," =  ", a*i)
+    if a*i <10:
+        print(a, " x ", i ," =  ", a*i,"\t\t", a+1, " x ", i ," =  ", (a+1)*i,"\t", a+2, " x ", i ," =  ", (a +2)*i,"\t" ,a+3, " x ", i ," =  ", (a+3)*i )
+    else:
+        print(a, " x ", i ," =  ", a*i,"\t", a+1, " x ", i ," =  ", (a+1)*i,"\t", a+2, " x ", i ," =  ", (a +2)*i,"\t" ,a+3, " x ", i ," =  ", (a+3)*i )
