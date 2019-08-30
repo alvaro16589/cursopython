213fcd230bc840d7af4bab48e7010b7342a156c0
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2019-08-30 17:55:58 -0400

    comentario

--- mayorD3.py
+++ mayorD3.py
@@ -1,6 +1,7 @@
 a = int(input("ingrese una valor: "))
 b = int(input("ingrese una valor: "))
 c = int(input("ingrese una valor: "))
+print("El mayor es :\t")
 if a>=b and a>=c:
     print (a)
 elif b>=a and b>=c:
